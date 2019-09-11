ab41e9615f001be74ec285e01ac053e694f93b2c
luhuibo <luhuibo306@foxmail.com>
2019-09-11 10:46:55 +0800

    【添加】config中的file_path添加将module添加到sys.path中

--- config/file_path.py
+++ config/file_path.py
@@ -6,11 +6,25 @@ import os
 # 需要设置linux或者windows的配置路径
 sys_path = r'C:\AI-NLP\NewsInfo-Auto-Extration'
 
+import sys
+
+# 将module 添加到sys.path中
+self_config_path = r'C:\AI-NLP\NewsInfo-Auto-Extration\config'
+self_database_path = r'C:\AI-NLP\NewsInfo-Auto-Extration\database'
+self_model_path = r'C:\AI-NLP\NewsInfo-Auto-Extration\model'
+self_similar_said_path = r'C:\AI-NLP\NewsInfo-Auto-Extration\similar_said'
+sys.path.append(self_config_path)
+sys.path.append(self_database_path)
+sys.path.append(self_model_path)
+sys.path.append(self_similar_said_path)
+
+# PYLTP路径的设置
 LTP_MODEL_PATH = sys_path + r'\data\ltp_data_v3.4.0'
 SYNONYMS_PATH = sys_path + r'\data\synonyms/synonyms.txt'
 DEFAULT_STOPWORDS_PATH = sys_path + r'\data\stop_words/stopwords.txt'
 WORD2VER_MODEL_PATH = sys_path + r'\data\word2vec100.wv'
 
+# 读取/保存文本的路径
 path_news_sentences_xut_txt = sys_path + r'\data\news-sentences-xut.txt'
 path_news_txt = sys_path + r'\data\news.txt'
 path_news_model = sys_path + r'\data\news_model'
